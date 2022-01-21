8cb6605cd05dca4b94dea3555a96fd6680f82183
Angus Gibson <angus@agibson.me>
2022-01-21 11:11:32 +1100

    Write compiler flags in manifest
    Due to a bug that arose from differing compiler flags,
    this seems like important data to catalogue.

--- mom_manifest.py
+++ mom_manifest.py
@@ -1,7 +1,7 @@
+import argparse
 import datetime
 import os
 import subprocess
-import sys
 import yaml
 
 from contextlib import contextmanager
@@ -16,33 +16,45 @@ def pushd(new_dir):
     finally:
         os.chdir(prev_dir)
 
-# srcdir = ../../src -> Path("../../src")
-srcdir = Path(
-    open("../config.ninja", "r")
-    .readline()
-    .split("=")[-1]
-    .strip()
+parser = argparse.ArgumentParser(description="Generate MOM6 build manifest")
+parser.add_argument("executable")
+parser.add_argument("manifest")
+parser.add_argument(
+    "--srcdir",
+    type=Path,
+    help="the MOM6 source directory containing subcomponents"
 )
+parser.add_argument(
+    "--fflags",
+    help="flags for compiling Fortran files"
+)
+parser.add_argument(
+    "--cflags",
+    help="flags for compiling C files"
+)
+args = parser.parse_args()
 
 component_revs = []
 for component in ["MOM6", "SIS2", "FMS"]:
-    with pushd(str(srcdir / component)):
+    with pushd(str(args.srcdir / component)):
         res = subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True, text=True)
         component_revs.append({"component": component, "ref": res.stdout.strip()})
 
 modules_res = subprocess.run("module list -t", shell=True, capture_output=True, text=True)
 modules = modules_res.stdout.strip().split("\n")[2:]
 
-hash_res = subprocess.run(["sha256sum", sys.argv[1]], capture_output=True, text=True)
+hash_res = subprocess.run(["sha256sum", args.executable], capture_output=True, text=True)
 file_hash = hash_res.stdout.strip().split()[0]
 
 data = {
-    "exe": sys.argv[1],
+    "exe": args.executable,
     "sha256": file_hash,
     "build-date": datetime.date.today(),
     "git": component_revs,
     "modules": modules,
+    "fflags": args.fflags,
+    "cflags": args.cflags,
 }
 
-with open(sys.argv[2], "w") as f:
-    yaml.dump(data, f)
+with open(args.manifest, "w") as f:
+    yaml.dump(data, f, sort_keys=False)
