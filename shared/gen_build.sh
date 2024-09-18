a8ebabaa808fa700bcdd1f710199d6b01e8b1f67
Angus Gibson <angus@agibson.me>
2024-09-18 15:47:11 +1000

    Build FMS with use_netCDF
    We need to provide this definition for correct behaviour.

--- shared/gen_build.sh
+++ shared/gen_build.sh
@@ -5,7 +5,7 @@
 # find include directories
 inc_dirs=($(find -L ${srcdir}/FMS -type d -name 'include') ${srcdir}/FMS/constants ${srcdir}/FMS/constants4 ${srcdir}/FMS/fms)
 inc_flags="$(printf -- "-I%s " "${inc_dirs[@]}")"
-cpp_defs="-Duse_deprecated_io"
+cpp_defs="-Duse_deprecated_io -Duse_netCDF"
 
 cat << EOF > build.ninja
 include ../config.ninja
