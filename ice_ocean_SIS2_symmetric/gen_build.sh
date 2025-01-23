77be503b6434814bc261386fc96ca96f628d2da1
Christopher Bull <5499680+chrisb13@users.noreply.github.com>
2025-01-23 15:51:44 +1100

    Update ice_ocean_symmetric file paths (#9)
    Need to look in slightly different locations.

--- ice_ocean_SIS2_symmetric/gen_build.sh
+++ ice_ocean_SIS2_symmetric/gen_build.sh
@@ -14,13 +14,15 @@ EOF
 
 # lists of source files
 fsrc_files=($(find -L ${srcdir}/MOM6/src -iname '*.f90'))
-fsrc_files+=($(find -L ${srcdir}/MOM6/config_src/infra/FMS2 -iname '*.f90'))
+fsrc_files+=($(find -L ${srcdir}/MOM6/config_src/infra/FMS1 -iname '*.f90'))
 fsrc_files+=($(find -L ${srcdir}/MOM6/config_src/external -iname '*.f90'))
 fsrc_files+=($(find -L ${srcdir}/MOM6/config_src/drivers/FMS_cap -iname '*.f90'))
 fsrc_files+=($(find -L ${srcdir}/SIS2 -iname '*.f90'))
 # coupler files
-fsrc_files+=($(find -L ${srcdir}/{atmos_null,coupler/full,coupler/shared,land_null,ice_param,icebergs/src} -iname '*.f90'))
+fsrc_files+=($(find -L ${srcdir}/{atmos_null,coupler,land_null,ice_param,icebergs/src} -iname '*.f90'))
 fsrc_files+=($(find -L ${srcdir}/FMS/coupler -iname '*.f90'))
+
+
 objs=()
 
 # build module provides for fortran files
