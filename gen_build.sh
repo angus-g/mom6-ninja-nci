02ceab1c3892a27d6cb2cf1c78665b516b621946
Angus Gibson <angus@agibson.me>
2025-09-30 11:18:56 +0100

    Use lld for linking
    I don't understand why this has broken, but without LTO builds take forever. With it, we need to force lld or the Intel compiler uses inconsistent object representations.
    
    Closes #12.

--- gen_build.sh
+++ gen_build.sh
@@ -20,13 +20,14 @@ EOF
     cat << 'EOF' >> config.ninja
 fc = mpif90
 cc = mpicc
-ld = mpifort
+ld = mpif90
 ar = ar
 
 fflags = -fno-alias -auto -safe-cray-ptr -ftz -assume byterecl -i4 -r8 -nowarn -g
 fflags_opt = $fflags -O2 -fp-model precise -qoverride-limits -xHost -traceback -flto
 fflags_dbg = $fflags -O0 -check -check noarg_temp_created -check nopointer -warn -warn noerrors -traceback -assume nobuffered_io
 cflags = -g -O2 -flto
+ldflags = -fuse-ld=lld
 
 arflags = rv
 
